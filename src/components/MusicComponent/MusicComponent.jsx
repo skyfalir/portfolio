7e418eb84774a99516934d2e1790f33e4de28a7e
skyfalir <skyflairofficial@gmail.com>
2024-03-11 00:02:39 +0100

    Final fixes

--- src/components/MusicComponent/MusicComponent.jsx
+++ src/components/MusicComponent/MusicComponent.jsx
@@ -6,7 +6,7 @@ const MusicComponent = () => {
   return (
     <div className="music-content">
       <div className="music-header">
-        <h1 className="artist-name">SkyFlair</h1>
+        <h2 className="artist-name">SkyFlair</h2>
         <p className="artist-paragraph">
           With almost 13 years of experience with music, I've explored a vast range of genres and styles.
           Music has been a passion of mine for as long as I can remember, and I'm always exploring new and exciting methods to express myself with sound.
